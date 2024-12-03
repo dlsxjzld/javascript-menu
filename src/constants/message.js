f308aa86974dea8396364ec6bc9cafa3638bf5f0
dlsxjzld <sinjuk1@naver.com>
2024-12-03 19:08:48 +0900

    feat: message.js에 못먹는 음식 메시지 상수 추가

--- src/constants/message.js
+++ src/constants/message.js
@@ -1,6 +1,7 @@
 const MESSAGE = {
-  INSTRUCTION: '점심 메뉴 추천을 시작합니다.',
-  COACH_INPUT: '코치의 이름을 입력해 주세요. (, 로 구분)',
+  INSTRUCTION: '점심 메뉴 추천을 시작합니다.\n',
+  COACH_INPUT: '코치의 이름을 입력해 주세요. (, 로 구분)\n',
+  ASK_FOOD_INPUT: '(이)가 못 먹는 메뉴를 입력해 주세요.\n',
 };
 
 module.exports = MESSAGE;
