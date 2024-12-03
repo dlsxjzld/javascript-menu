910418d7e92c5c8e3e0978bbc5c521789d95644b
dlsxjzld <sinjuk1@naver.com>
2024-12-03 18:39:03 +0900

    feat: OutputView에서 점심 메뉴 추천 시작 문구 출력 구현

--- src/view/OutputView.js
+++ src/view/OutputView.js
@@ -1,6 +1,12 @@
 const { Console } = require('@woowacourse/mission-utils');
+const MESSAGE = require('../constants/message.js');
 
 const OutputView = {
+  printInstruction() {
+    Console.print(MESSAGE.INSTRUCTION);
+    Console.print('');
+  },
+
   printResult(result) {
     Console.print(result);
   },
