2cd7166f22708d1fc13039b27f7520128d95960a
dlsxjzld <sinjuk1@naver.com>
2024-12-13 14:12:47 +0900

    feat: 검증에 필요한 에러메시지 상수 추가

--- src/constants/message.js
+++ src/constants/message.js
@@ -8,8 +8,11 @@ export const MESSAGE = {
 
 export const ERROR_MESSAGE = {
   INVALID: '유효하지 않은 입력입니다.',
+  INVALID_EMPTY_STRING: '빈 문자열은 안됩니다.',
   INVALID_COACH_NAME_LENGTH: '코치의 이름은 최소 2글자, 최대 4글자입니다.',
   INVALID_COACH_COUNT: '코치는 최소 2명 이상 입력해야 합니다.',
+  INVALID_DUPLICATE_NAME: '중복된 이름은 안됩니다.',
   INVALID_MENU: '존재하지 않는 메뉴입니다.',
   INVALID_MENU_COUNT: '못 먹는 메뉴는 0~2개 입니다.',
+  INVALID_DUPLICATE_MENU: '중복된 음식은 안됩니다.',
 };
