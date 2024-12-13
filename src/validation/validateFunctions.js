7a96c43f9ea407648109ea8d7d94acb6dd1a422e
dlsxjzld <sinjuk1@naver.com>
2024-12-13 10:46:41 +0900

    feat: 코치 이름 입력 검증 추가

--- src/validation/validateFunctions.js
+++ src/validation/validateFunctions.js
@@ -20,6 +20,25 @@ const isEmptyString = (input) => {
     `${ERROR_MESSAGE.INVALID} 빈 문자열은 안됩니다.`,
   );
 };
+const checkCoachesNameLength = (input) => {
+  toThrowNewError(
+    input
+      .split(',')
+      .some(
+        (coach) =>
+          coach.length < CONSTANT.NAME_MIN_LENGTH ||
+          coach.length > CONSTANT.NAME_MAX_LENGTH,
+      ),
+    ERROR_MESSAGE.INVALID_COACH_NAME_LENGTH,
+  );
+};
+const checkCoachesCount = (input) => {
+  const coaches = input.split(',').filter(Boolean).length;
+  toThrowNewError(
+    coaches.length < CONSTANT.COUNT_MIN || coaches.length > CONSTANT.COUNT_MAX,
+    ERROR_MESSAGE.INVALID_COACH_NAME_LENGTH,
+  );
+};
 
 const checkInteger = (input) => {
   const bridgeSize = Number(input);
@@ -28,17 +47,12 @@ const checkInteger = (input) => {
     `${ERROR_MESSAGE.INVALID} 정수만 가능합니다.`,
   );
 };
-const checkBridgeRange = (input) => {
-  const bridgeSize = Number(input);
-  toThrowNewError(
-    bridgeSize < CONSTANT.BRIDGE_MIN || bridgeSize > CONSTANT.BRIDGE_MAX,
-    `${ERROR_MESSAGE.INVALID} `,
-  );
-};
 
-export const validateSomething = (input) => {
+export const validateCoaches = (input) => {
   hasEmptySpace(input);
   isEmptyString(input);
+  checkCoachesNameLength(input);
+  checkCoachesCount(input);
 };
 // export const validateSomething = (input) => {
 //   hasEmptySpace(input);
