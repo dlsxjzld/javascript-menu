8145cd7321c67171f7c5b4d096e9a28f31b97fd6
dlsxjzld <sinjuk1@naver.com>
2024-12-13 10:46:27 +0900

    feat: 검증에 필요한 에러메시지 상수 추가

--- src/constants/message.js
+++ src/constants/message.js
@@ -7,7 +7,8 @@ export const MESSAGE = {
 };
 
 export const ERROR_MESSAGE = {
-  INVALID: '다리 길이는 3부터 20 사이의 숫자여야 합니다.',
-  INVALID_COACH_NAME: '코치는 최소 2명 이상 입력해야 합니다.',
+  INVALID: '유효하지 않은 입력입니다.',
+  INVALID_COACH_NAME_LENGTH: '코치의 이름은 최소 2글자, 최대 4글자입니다.',
+  INVALID_COACH_COUNT: '코치는 최소 2명 이상 입력해야 합니다.',
   INVALID_MENU: '존재하지 않는 메뉴입니다.',
 };
