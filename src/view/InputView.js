c5f6473cac4d1fe386952655daa0dd5970b69801
dlsxjzld <sinjuk1@naver.com>
2024-12-13 10:34:45 +0900

    feat: 코치 이름, 못 먹는 메뉴 입력 추가

--- src/view/InputView.js
+++ src/view/InputView.js
@@ -1,32 +1,14 @@
 import { Console } from '@woowacourse/mission-utils';
 import { MESSAGE } from '../constants/message.js';
 
-/**
- * 사용자로부터 입력을 받는 역할을 한다.
- */
 export const InputView = {
-  /**
-   * 다리의 길이를 입력받는다.
-   */
-  async readBridgeSize() {
-    const input = await Console.readLineAsync(MESSAGE.ASK_BRIDGE_SIZE);
-    Console.print('');
+  async readCoachName() {
+    const input = await Console.readLineAsync(MESSAGE.ASK_COACH_NAME);
     return input;
   },
 
-  /**
-   * 사용자가 이동할 칸을 입력받는다.
-   */
-  async readMoving() {
-    const input = await Console.readLineAsync(MESSAGE.ASK_MOVE);
-    return input;
-  },
-
-  /**
-   * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
-   */
-  async readGameCommand() {
-    const input = await Console.readLineAsync(MESSAGE.ASK_GAME_TRY);
+  async readCantEat(coach) {
+    const input = await Console.readLineAsync(MESSAGE.ASK_CANT_EAT(coach));
     return input;
   },
 
